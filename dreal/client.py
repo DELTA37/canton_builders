456528108fdb85a93274739fb8628cb76c50a10f
George Kasparyants <gg_plastt@mail.ru>
2025-11-28 12:58:15 +0300

    fix canton net

--- dreal/client.py
+++ dreal/client.py
@@ -1,8 +1,8 @@
 import os
-from dazl import connect
+from typing import Optional
+
 from .settings import LEDGER_URL
-import asyncio, json
-from dazl import connect
+import dazl
 from .types import RealEstateMeta
 
 
@@ -10,16 +10,19 @@ class RealEstateClient:
     def __init__(self,
                  registrar="Registrar"):
         self.ledger_url = LEDGER_URL
-        self.registrar = registrar
-        self.conn = None
+        self.registrar = dazl.Party(registrar)
+        self.conn: Optional[dazl.Connection] = None
 
     async def __aenter__(self):
-        self.conn = await connect(url=self.ledger_url,
-                                  act_as=[self.registrar])
+        self.conn = await dazl.connect(url=self.ledger_url,
+                                       act_as=[self.registrar]).__aenter__()
+        # print(await self.conn.get_version())
+        parties = await self.conn.list_known_parties()
+        print(parties)
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.conn.close()
+        await self.conn.__aexit__(exc_type, exc_val, exc_tb)
 
     # Создание объекта недвижимости
     async def create_property(self, property_id: str, owner: str, meta: RealEstateMeta):
@@ -53,5 +56,6 @@ class RealEstateClient:
 
     # Получение всех активных объектов недвижимости
     async def list_properties(self):
-        active = await self.conn.find("RealEstate.RealEstate")
-        return [c.payload for c in active]
+        async with self.conn.query_many('*') as stream:
+            async for event in stream.creates():
+                print(event.contract_id, event.payload)
--- dreal/settings.py
+++ dreal/settings.py
@@ -1,4 +1,7 @@
 import os
+import dotenv
 
+dotenv.load_dotenv()
 
 LEDGER_URL = os.environ.get("LEDGER_URL")
+print(LEDGER_URL)
